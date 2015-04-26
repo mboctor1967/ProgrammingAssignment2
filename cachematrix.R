70ba6c4319e93b8eb662accd1a05402833197b74
mboctor1967 <mboctor@gmail.com>
2015-04-26 22:03:00 +1000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv = NULL
+  
+  set = function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get = function() x
+  setinv = function(inverse) inv <<- inverse 
+  getinv = function() inv
+  
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## return's inverse of the original matrix input to makeCacheMatrix()
+  inv = x$getinv()  
+  if (!is.null(inv)){
+    return(inv)
+  }
+  
+  # calculates the inverse
+  my_matrix.data = x$get()
+  inv = solve(my_matrix.data, ...)
+  
+  # sets the value of the inverse in the cache via the setinv function.
+  x$setinv(inv)
+  return(inv)
 }
+
+
+# run function calling makeCacheMatrix twice
+run = function(my_matrix){
+  
+  temp_matrix = makeCacheMatrix(my_matrix)
+  
+  time.start = Sys.time()
+  cacheSolve(temp_matrix)
+  time.end = Sys.time()
+  elapsed_time = time.end - time.start
+  print(elapsed_time)
+  
+  time.start = Sys.time()
+  cacheSolve(temp_matrix)
+  time.end = Sys.time()
+  elapsed_time = time.end - time.start
+  print(elapsed_time)
+}
+
+# call run function passing it a 1000x1000 matrix of randon nummbers
+run (matrix(rnorm(250000), nrow=500, ncol=500))
